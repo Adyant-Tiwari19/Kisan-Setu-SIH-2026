9ff81e600afd2a109d771cc5d97490a55d19a3bd
Adyant-Tiwari19 <adyanttiwari19@gmail.com>
2026-08-27 20:13:44 +0530

    Added auth endpoint for registering new users

---
+++ backend/app/api/v1/endpoints/auth.py
@@ -0,0 +1,34 @@
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user_schema import UserCreate, UserResponse
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+@router.post(
+    "/register",
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED
+)
+def register_user(user_in: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.phone == user_in.phone).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this phone number is already registered."
+        )
+
+    user = User(
+        name = user_in.name,
+        phone= user_in.phone,
+        role= user_in.role,
+        address= user_in.address,
+        pincode= user_in.pincode,
+        account_num= user_in.account_num,
+        ifsc= user_in.ifsc
+    )
+    db.add(User)
+    db.commit()
+    db.refresh(user)
+    return user
---
+++ backend/app/api/v1/router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.api.v1.endpoints import ai_services , auth , listings, orders
+
+api_router = APIRouter()
+api_router.include_router(auth.router , prefix="/auth", tags = ["Authentication"])
+api_router.include_router(
+    listings.router, prefix="/listings", tags=["Listings"]
+)
+api_router.include_router(orders.router, prefix=["Orders"], tags=["Orders"])
+api_router.include_router(
+    ai_services.router , prefix="/ai", tags=["AI & Optimization"]
+)
